8af47e9c92e38443c2bef2c0a53a64898d68b8e6
Fo-lem <darksunset2001@gmail.com.com>
2023-10-23 18:44:10 +0300

    Setting up the database and creating an .env file

---
+++ bd.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize')
+
+module.exports = new Sequelize(
+  import.meta.env.DB_NAME,
+  import.meta.env.DB_USER,
+  import.meta.env.DB_PASSWORD,
+  {
+    dialect: 'postgres',
+    host: import.meta.env.DB_HOST,
+    post: import.meta.env.DB_PORT,
+  },
+
+)
