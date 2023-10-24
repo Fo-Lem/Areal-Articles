f8f644fa54fb116eb00df643c5c337c7ddb327a9
Fo-lem <darksunset2001@gmail.com.com>
2023-10-24 12:08:42 +0300

    fixed imports for modules and first start

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
+import process from 'node:process'
 import express from 'express'
 import cors from 'cors'
-import sequelize from './db'
-
-import { articleRouters } from './routes/articlesRoutes.js'
-import { commentsRouter } from './routes/commentsRoutes'
+import { bd } from './bd.js'
+import { articlesRouter } from './routes/articlesRoutes.js'
+import { commentsRouter } from './routes/commentsRoutes.js'
 
 const app = express()
-const PORT = import.meta.env.PORT || 8081
+const PORT = process.env.PORT || 8081
 
 app.use(cors())
 app.use(express.json())
 
-app.use('/', articleRouters)
+app.use('/', articlesRouter)
 app.use('/', commentsRouter)
 
 async function start() {
   try {
-    await sequelize.authenticate()
-    await sequelize.sync()
+    await bd.authenticate()
+    await bd.sync()
     app.listen(PORT, () => {
       return console.log(`server started on port ${PORT}`)
     })
