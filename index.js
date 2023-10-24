411bbfdc6a3ef60471ec29232fee30f3026af402
Fo-lem <darksunset2001@gmail.com.com>
2023-10-24 11:11:09 +0300

    added use routers

--- index.js
+++ index.js
@@ -2,25 +2,18 @@ import express from 'express'
 import cors from 'cors'
 import sequelize from './db'
 
+import { articleRouters } from './routes/articlesRoutes.js'
+import { commentsRouter } from './routes/commentsRoutes'
+
 const app = express()
 const PORT = import.meta.env.PORT || 8081
-
-// const routerUser = require("./routes/User.routes");
-// const routerMessage = require("./routes/Message.routes");
-// const routerWs = require("./routes/Ws.routes");
-// const routerRooms = require("./routes/Rooms.routes");
-// const errorHandler = require("./middleware/ErrorHandlingMiddleware");
 
 app.use(cors())
 app.use(express.json())
 
-// app.use("/api", routerWs);
-// app.use("/api", routerUser);
-// app.use("/api", routerMessage);
-// app.use("/api", routerRooms);
+app.use('/', articleRouters)
+app.use('/', commentsRouter)
 
-// //Обработка ошибок
-// app.use(errorHandler)
 async function start() {
   try {
     await sequelize.authenticate()
